8211d10d127109493350ee0beb4c8caba42f4a94
DhwaniiPatel <69194518+DhwaniiPatel@users.noreply.github.com>
2024-06-14 12:34:20 +0930

    Added comments and rearranged functions

--- _targets.R
+++ _targets.R
@@ -31,20 +31,20 @@ list(
   tar_file(gene_data, "raw-data/2024-03-19-WIF-tis4d.csv"),
   # clean data
   tar_target(gene_data_clean, clean_data(gene_data)),
-  tar_target(gene_cell1, clean_data2(gene_data_clean)),
-  tar_target(gene_cell2, clean_data3(gene_data_clean)),
-  # table 1
+  tar_target(gene_cell1, clean_data2(gene_data_clean, 'Wild-Type')),
+  tar_target(gene_cell2, clean_data2(gene_data_clean, 'Cell-Type 101')),
+  # table 1: groups of cell lines and treatments
   tar_target(categories, get_table(gene_data_clean)),
-  # plot 1
+  # plot 1: relationships between cell concentration and gene expression
   tar_target(gene_plot, plot_scatter(gene_cell1, gene_cell2)),
-  # table 2
+  # table 2: growth rates of gene expression
   tar_target(growth, get_table_growth(gene_data_clean)),
-  # model
+  # model: mixed effects with random intercept
   tar_target(model_output, fit_model(gene_data_clean)),
-  # output
+  # output: results of the model
   tar_target(results, model_results(model_output)),
-  # plot 2
+  # plot 2: residual of predicted
   tar_target(residual_plot, plot2(model_output, gene_data_clean)),
-  # readme file - to be completed
+  # readme file
   tar_quarto(readme, "readme.qmd")
 )
